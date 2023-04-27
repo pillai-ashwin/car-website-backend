3494e45dbbfabb0f42223294ecd5d88f195a66fb
Ashwin Pillai <13049220+pillai-ashwin@users.noreply.github.com>
2023-04-27 17:16:04 -0400

    add additional test to pricing-service

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -2,15 +2,31 @@ package com.udacity.pricing;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PricingServiceApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+	
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testGetPrice() throws Exception {
+		mvc.perform(get("/services/price").param("vehicleId", "1"))
+				.andExpect(status().isOk())
+		.andExpect(jsonPath("vehicleId").value("1"))
+		.andExpect(jsonPath("currency").value("USD"));
 	}
 
 }
